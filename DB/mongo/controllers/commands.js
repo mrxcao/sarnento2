ef9c816e71d123569ae380474badc9ec6706a47f
MrXcao <francisco.ramos@gmail.com>
2023-02-01 17:51:21 -0300

    more schema validate

--- DB/mongo/controllers/commands.js
+++ DB/mongo/controllers/commands.js
@@ -8,24 +8,23 @@ class CommandsController {
 	}
 	async index(query = {}) {
 		const ret = await model.find(query) ;
-		// return res.status(200).json(ret);
 		return ret;
 	}
 	async show(_id) {
 		const ret = await model.findById(_id) ;
-		// return res.status(200).json(ret);
 		return ret;
 	}
-	async update() {
-		//
+	async update(_id, req) {
+		const ret = await model.findByIdAndUpdate(_id, req);
+		return ret;
 	}
-	async destroy() {
-		//
+	async destroy(_id) {
+		const ret = await model.findByIdAndDelete(_id);
+		return ret;
 	}
 	async upSert(req) {
 		const query = { 'nome':req.nome };
 		const ret = model.findOneAndUpdate(query, req, { upsert: true });
-		// return res.status(200).json(ret);
 		return ret;
 	}
 }
--- DB/mongo/models/commands.js
+++ DB/mongo/models/commands.js
@@ -4,7 +4,7 @@ const objectId = Schema.ObjectId;
 
 const _schema = new Schema({
 	id: objectId,
-	nome: String,
+	nome: { type: String, required: true, unique:true, lowercase: true, trim: true },
 	prefix: { type: Boolean, default: false },
 	slash: { type: Boolean, default: false },
 	criado: { type: Date, default: Date.now },
