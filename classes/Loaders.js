2e0d718ddcac5a676baa9450cf534a8d23adf1ba
MrXcao <francisco.ramos@gmail.com>
2023-02-03 18:39:01 -0300

    init express server

--- classes/Loaders.js
+++ classes/Loaders.js
@@ -1,6 +1,11 @@
 const mongo = require('../DB/mongo/connect');
+const server = require('../server/init');
 class Loaders {
-	async init() {await mongo.connect();}
+	async init() {
+		await mongo.connect();
+		await server.init();
+	}
+
 }
 
 module.exports = new Loaders();
