660defecd1b1ba7559812075e8aa43eb8dce2bf0
MrXcao <francisco.ramos@gmail.com>
2023-02-08 16:25:26 -0300

    guild ´publ and chapeuSeletor

--- commands/ban.js
+++ commands/ban.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 module.exports = {
 	data : new SlashCommandBuilder()
 		.setName('ban')
-		.setDescription('Select a member and ban them.')
+		.setDescription('Manda o safado pastar.')
 		.addUserOption(option =>
 			option
 				.setName('target')
