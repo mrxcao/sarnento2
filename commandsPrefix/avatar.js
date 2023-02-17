b9a5cdbb849fe05d5580fdddb889bbf1e001b629
MrXcao <francisco.ramos@gmail.com>
2023-02-17 20:08:56 -0300

    global commands publ and avatares

--- commandsPrefix/avatar.js
+++ commandsPrefix/avatar.js
@@ -1,20 +1,25 @@
 const usersCtl = require('../DB/mongo/controllers/users');
 
 module.exports = async (client, msg) => {
-	// msg.reply(msg.author.displayAvatarURL());
-	// msg.channel.send( msg.author.displayAvatarURL() );
 	const usrs = await usersCtl.index();
-
 	let text = '';
-
+	const t = [];
 	for (const u of usrs) {
-		text = text + 'https://cdn.discordapp.com/avatars/217299114843439115/5d057b7075eee92dd985f6e6d5dca0d3.web' + u.avatar + ' ';
-		console.log('u', u.avatar);
+		text = text + u.avatar + ' ';
+		if (u.avatar) {
+			t.push({
+				attachment: u.avatar,
+				// name: u.username,
+			});
+		}
 	}
+	msg.channel.send({
+		files: t,
+		content: 'avatares mil',
+	});
 
-	console.log('usrs', text);
-	console.log('msg.author.displayAvatarURL()', msg.author.displayAvatarURL());
-	msg.reply(text + ' ');
+	/*
 	msg.reply(msg.author.displayAvatarURL());
 	msg.reply(msg.author.avatarURL({ dynamic:true }));
+	*/
 };
