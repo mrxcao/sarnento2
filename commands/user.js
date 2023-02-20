ad9ebe549fddbf9ed7288a13e5b0501b9cfcc6ac
Francisco Ramos <francisco.ramos@gmail.com>
2023-02-20 18:38:55 -0300

    try to create a pokecard

--- commands/user.js
+++ commands/user.js
@@ -77,11 +77,9 @@ client.on(Events.InteractionCreate, async interaction => {
 const applyText = (canvas, text) => {
 	const context = canvas.getContext('2d');
 	let fontSize = 70;
-
 	do {
 		context.font = `${fontSize -= 10}px sans-serif`;
 	} while (context.measureText(text).width > canvas.width - 300);
-
 	return context.font;
 };
 
