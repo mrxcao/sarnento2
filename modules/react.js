6a405d991c56f9b00ef7f8fc7bf5ba85e315e20d
MrXcao <francisco.ramos@gmail.com>
2023-04-12 16:11:35 -0300

    clean code

--- modules/react.js
+++ modules/react.js
@@ -57,18 +57,6 @@ const verify = async (args, msg) => {
 	}
 
 
-	// //////////////////////////
-/*
-	const arr = [{ chave:'cachorro', 'value':'AU AU!' }];
-	for (const w of arr) {
-		if (args.find(e => e.toLowerCase() == w.chave.toLowerCase() ||
-    String(e.toLowerCase()).indexOf(w.chave.toLowerCase()) > -1)) {
-			msg.reply({ content: w.value,
-				// tts:true,
-			});
-		}
-	}
-	*/
 };
 
 load();
