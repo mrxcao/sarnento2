fa098549e3ad15053b96da8b19d1c97ad3b7f892
MrXcao <francisco.ramos@gmail.com>
2023-08-09 09:52:28 -0300

    qtde test

--- commands/poke.js
+++ commands/poke.js
@@ -17,6 +17,7 @@ module.exports = {
 		),
 	async execute(interaction) {
 		let qtde = parseInt(interaction.options.getInteger('quantos'));
+		console.log('qtde', qtde);
 		if (isNaN(qtde)) {qtde = 1080;}
 		pokeApi.quiz(interaction, qtde);
 		if (qtde <= 150) {
