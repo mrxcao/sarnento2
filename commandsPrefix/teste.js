cbcfa4ef0ed383378443364ba515fe4913f496fa
MrXcao <francisco.ramos@gmail.com>
2023-10-02 19:42:46 -0300

    teste bard

--- commandsPrefix/teste.js
+++ commandsPrefix/teste.js
@@ -1,4 +1,6 @@
-const openAI = require('../modules/apis/openAI');
-module.exports = (client, msg) => {
-	openAI.teste(msg);
+const bard = require('../modules/apis/google');
+
+module.exports = async (client, msg) => {
+	const resp = await bard.getAnswer('Qual é o limite do prompt em tokens que o google bard suporta? Pode fazer uma comparação com outras AIs?	faça gráficos e tabelas em markdown');
+	msg.reply(resp);
 };
--- modules/actions.js
+++ modules/actions.js
@@ -1,10 +1,12 @@
-const openAI = require('./apis/openAI');
+// const openAI = require('./apis/openAI');
+const google = require('./apis/google');
 const log = require('./log');
 
 const responder = async (msg, pergunta) => {
-	const res = await openAI.perguntar(msg, pergunta);
+	// const res = await openAI.perguntar(msg, pergunta);
+	const res = await google.getAnswer(msg, pergunta);
 	msg.reply(res);
-	log.action(msg, 'openAI', pergunta, res);
+	log.action(msg, 'bard', pergunta, res);
 	return res;
 };
 
