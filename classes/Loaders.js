badd86ec483e96fa41c83487e078684dc2c265e7
MrXcao <francisco.ramos@gmail.com>
2023-10-09 14:39:48 -0300

    no more api here

--- classes/Loaders.js
+++ classes/Loaders.js
@@ -1,11 +1,8 @@
 const mongo = require('../DB/mongo/connect');
-const server = require('../src/init');
+
 class Loaders {
 	async init() {
 		await mongo.connect();
-		// if (process.env.NODE_ENV == 'development') {
-		await server.init();
-		// }
 	}
 
 }
