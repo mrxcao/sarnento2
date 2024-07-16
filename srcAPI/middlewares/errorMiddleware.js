a9133d54ba595f013aa3df9889e51dc571e4ea41
Francisco Ramos <francisco.ramos@gmail.com>
2024-07-16 12:01:31 -0300

    error middle

--- srcAPI/middlewares/errorMiddleware.js
+++ srcAPI/middlewares/errorMiddleware.js
@@ -1,5 +1,5 @@
 module.exports = ((error, req) => {
-	const err = `${error.method} ${error.url} ${error.ip} ${error.message} ${error.erro} ${req}`;
+	const err = `${error.method} ${error.url} ${error.ip} ${error.message} ${error.erro} ${   JSON.stringify(req)  }`;
 	// console.error('||  error', err);
 	throw new Error(err);
 });
