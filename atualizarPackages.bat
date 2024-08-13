416dc2b218a5c27c70fab567e0174a4c26f810d3
Francisco Ramos <francisco.ramos@gmail.com>
2024-08-13 10:15:38 -0300

    atualiza scrips

--- atualizarPackages.bat
+++ atualizarPackages.bat
@@ -1,3 +1,2 @@
-rm -rf node_modules package-lock.json
 rmdir /s /q node_modules && del /f /q package-lock.json
 npm install
---
+++ atuallizaPackages.sh
@@ -0,0 +1,3 @@
+rm -rf node_modules
+rm -f package-lock.json
+npm install
