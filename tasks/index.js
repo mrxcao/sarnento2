1d6afefbe047e45daf7f766b2f5538171a3aa5cf
Francisco Ramos <francisco.ramos@gmail.com>
2025-12-01 14:52:35 -0300

    crom

--- tasks/index.js
+++ tasks/index.js
@@ -6,20 +6,22 @@ const schedule = require('node-schedule');
 const tasklist = require('./tasks');
 const mongodb = require('./modules/DB/mongo/connect');
 
+function stringifyLimited(obj, maxLen = 80) {
+  const str = JSON.stringify(obj);
+  return str.length > maxLen ? `${str.substring(0, maxLen)}...` : str;
+}
+
 const run = async () => {
-  /*
-  if (!mongo.isConnected()) {
-    mongo.connect();
-  }
-  */
   await mongodb.connect();
-  let c = 1;
   // eslint-disable-next-line no-restricted-syntax
   for (const task of tasklist) {
     schedule.scheduleJob(task.schedule, task.callback);
-    console.log(c, task.name);
-    c += 1;
+    // console.log(c, task.name, task.schedule);
   }
+  console.table(tasklist.map((t) => ({
+    name: t.name,
+    schedule: stringifyLimited(t.schedule),
+  })));
 
   console.log('Waiting for new taks  - ', process.env.NODE_ENV);
 };
